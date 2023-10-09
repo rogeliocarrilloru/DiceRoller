e901c775cf0730dcdd155d1f4c56d57fed55059e
rogeliocarrilloru <rrcr1975@gmail.com>
2023-10-10 00:47:55 +0200

    button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.diceroller"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.diceroller"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
